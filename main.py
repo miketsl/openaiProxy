6b4e0ec2221f37f9fa4e9e40c87b7a53a94ccf5e
Mike <mknott@undercurrentcapital.com>
2023-07-30 13:10:34 +0800

    Fixed the output to match openAI

--- main.py
+++ main.py
@@ -5,18 +5,49 @@ from EdgeGPT.EdgeGPT import Chatbot,ConversationStyle # Import the Chatbot class
 app = Flask(__name__)
 
 # OpenAI chat completion endpoint
-@app.route('/v1/chat/completion', methods=['POST'])
+@app.route('/v1/chat/completions', methods=['POST'])
 @app.route('/chat/completions', methods=['POST'])
 async def chat_completions():
+    print("in chat completions")
     data=request.json
+    print(data)
     messages=data.get('messages')
     prompt=""
     for message in messages:
         prompt+=f"{message['role']}: {message['content']}\n"
     # Generate a response using the ask method of the Chatbot instance 
     response = await bot.ask(prompt=prompt, conversation_style=ConversationStyle.creative, simplify_response=False) 
-    # Return the response as JSON 
-    return jsonify(response=response)
+    messages_left = response["item"]["throttling"]["maxNumUserMessagesInConversation"] - response["item"]["throttling"].get("numUserMessagesInConversation",0)
+    if messages_left == 0:
+        raise Exception("Max messages reached")
+    for msg in reversed(response["item"]["messages"]):
+        if msg.get("adaptiveCards") and msg["adaptiveCards"][0]["body"][0].get("text"):
+            message = msg
+            break
+
+    response = {
+        "choices": [
+            {
+                "finish_reason": "stop",
+                "index": 0,
+                "message": {
+                    "content": message["text"],
+                    "role": "assistant"
+                }
+            }
+        ],
+        "created": 1677664795,
+        "id": "chatcmpl-7QyqpwdfhqwajicIEznoc6Q47XAyW",
+        "model": "gpt-3.5-turbo-0613",
+        "object": "chat.completion",
+        "usage": {
+            "completion_tokens": 17,
+            "prompt_tokens": 57,
+            "total_tokens": 74
+        }
+    }
+    print(response)
+    return jsonify(response)
 
 # OpenAI edit completion endpoint
 #@app.route('/v1/edits', methods=['POST'])
